742052f7e32de8ba1cb5b1a5e275d0703bdeddca
Mateusz <mateuszkasztura@gmail.com>
2025-01-30 11:22:08 +0100

    fix db config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,17 +1,22 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+config();
 
-export const DATABASE_CONFIG: TypeOrmModuleAsyncOptions  = {
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-    }),
-};
+const configService = new ConfigService();
+
+const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: configService.get<string>('DB_HOST'),
+    port: parseInt(configService.get<string>('DB_PORT'), 5432),
+    username: configService.get<string>('DB_USER'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    synchronize: false,
+    entities: ['dist/**/*.entity.ts'],
+    migrations: ['dist/database/migrations/*.ts'],
+    migrationsRun: false,
+    logging: true,
+});
+
+export default AppDataSource;
