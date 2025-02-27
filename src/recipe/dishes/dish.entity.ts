3ac5da88118a6c0a80792c6236b4d6aa545413c0
Mateusz <mateusz.kajsztura@bannerflow.com>
2025-02-27 10:47:12 +0100

    Add dish migration

--- src/recipe/dishes/dish.entity.ts
+++ src/recipe/dishes/dish.entity.ts
@@ -1,12 +1,5 @@
 import { User } from 'src/user/user.entity';
-import {
-    BaseEntity,
-    Column,
-    Entity,
-    ManyToOne,
-    OneToMany,
-    PrimaryGeneratedColumn,
-} from 'typeorm';
+import { BaseEntity, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Ingredient } from '../ingredients/ingerdient.entity';
 
 @Entity()
@@ -17,11 +10,23 @@ export class Dish extends BaseEntity {
     @Column({ type: 'varchar' })
     name: string;
 
+    @Column({ type: 'varchar' })
+    slug: string;
+
+    @Column({ type: 'int' })
+    servings: number;
+
     @Column({ nullable: true, type: 'text' })
     description?: string;
 
     @Column({ type: 'boolean' })
     isPublic: boolean;
+
+    @Column({type: 'timestamp'})
+    createdAt: Date
+
+    @Column({type: 'timestamp'})
+    updatedAt: Date
 
     @ManyToOne(() => User, (user) => user.dishes)
     user: User;
