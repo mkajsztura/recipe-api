cb026eb4d8d5a41744699305301399ef2ae6fbf0
Mateusz <mateusz.kajsztura@bannerflow.com>
2025-04-09 16:02:40 +0200

    Add password validation with custom validator

--- src/decorators/match.decorator.ts
+++ src/decorators/match.decorator.ts
@@ -22,5 +22,5 @@ export function Match<T>(
                 },
             },
         })
-    }TypeScript decorators are a special kind of declaration that can be attached to a class declaration, method, accessor, property, or parameter. Decorators use the form @expression, where expression must evaluate to a function that will be called at runtime with information about the decorated declaration
+    }
 }
